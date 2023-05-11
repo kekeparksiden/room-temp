cc02952f6da4daeb70c5e28b86eb1c7f1ac9ddf3
kekeparksiden <132968762+kekeparksiden@users.noreply.github.com>
2023-05-10 22:53:00 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const fs = require('fs');
+require('dotenv').config();
+const path = require('path');
+const crypto = require('crypto');
+
+const express = require('express');
+const multer = require('multer');
+const port = process.env.PORT || 3000;
+
+const app = express();
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './');
+    },
+    filename: function (req, file, cb) {
+      // especifica el nombre del archivo deseado aquí
+      cb(null, req.url.split("/")[2]);
+    }
+});
+  
+const upload = multer({ storage: storage,
+    fileFilter: function (req, file, cb) {
+        setTimeout(() => {
+          fs.unlink(path.join(__dirname, './' + req.url.split("/")[2]), function (err) {
+            if (err) console.log(err);
+          });
+        }, 60000);
+        cb(null, true);
+} });
+
+app.post('/room/:room', upload.single('file'), (req, res) => {
+    const f = fs.readFileSync(path.join(__dirname, './' + req.url.split("/")[2]));
+    const hash = crypto.createHash('md5').update(f).digest('hex');
+    res.send(hash);
+});   
+
+app.get('/room/:room', (req, res) => {
+    res.sendFile(path.join(__dirname, './' + req.url.split("/")[2]));
+});
+
+app.listen(port, () => console.log(`Started in port=${port}`));
